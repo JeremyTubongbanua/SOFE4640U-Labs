c0ee983dde8d202ae572c0c7cb94b76d77c546e4
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2024-09-28 15:36:56 -0400

    feat: everything

---
+++ NoteMe2/app/src/main/java/com/sofe4640u/noteme/MainActivity.java
@@ -0,0 +1,26 @@
+package com.sofe4640u.noteme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button addNewNoteButton = findViewById(R.id.button);
+
+        addNewNoteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NewNote.class);
+                startActivity(intent);  // Starts NewNoteActivity
+            }
+        });
+    }
+}
