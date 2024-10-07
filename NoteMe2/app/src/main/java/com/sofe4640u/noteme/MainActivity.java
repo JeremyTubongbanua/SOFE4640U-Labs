d5d81d4540682d1a04aa6884eb14d6a9d1ab52b5
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2024-10-07 00:17:38 -0400

    feat: working colours

--- NoteMe2/app/src/main/java/com/sofe4640u/noteme/MainActivity.java
+++ NoteMe2/app/src/main/java/com/sofe4640u/noteme/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
@@ -61,14 +62,16 @@ public class MainActivity extends AppCompatActivity {
                 0);
 
         adapter.setViewBinder((view, cursor1, columnIndex) -> {
-            if (columnIndex == cursor1.getColumnIndex("COLOUR_NAME")) {
-                String colourName = cursor1.getString(cursor1.getColumnIndex("COLOUR_NAME"));
-                int color = getColorFromName(colourName);
-                RelativeLayout layout = (RelativeLayout) view.getParent();
-                layout.setBackgroundColor(color);
-                return true;
-            }
+            String name = String.valueOf(cursor1.getColumnIndex("COLOUR_NAME"));
+
+            String colourName = cursor1.getString(4);
+            int color = getColorFromName(colourName);
+            RelativeLayout layout = (RelativeLayout) view.getParent();
+            layout.setBackgroundColor(color);
+            Log.d("column isf", colourName);
+
             return false;
+
         });
 
         notesListView.setAdapter(adapter);
--- NoteMe2/app/src/main/java/com/sofe4640u/noteme/NewNote.java
+++ NoteMe2/app/src/main/java/com/sofe4640u/noteme/NewNote.java
@@ -3,10 +3,12 @@ package com.sofe4640u.noteme;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
 
